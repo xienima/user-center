f5bbec62524974549cb7b9c5f3f12f8cc0a4835a
xienima <2055472756@qq.com>
2022-08-28 17:53:09 +0800

    2.11-12 已经创建完项目并且完成了初步的mybatis自动化了

--- src/main/java/com/itmuch/usercenter/UserCenterApplication.java
+++ src/main/java/com/itmuch/usercenter/UserCenterApplication.java
@@ -2,7 +2,9 @@ package com.itmuch.usercenter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import tk.mybatis.spring.annotation.MapperScan;
 
+@MapperScan("com.itmuch")
 @SpringBootApplication
 public class UserCenterApplication {
 
